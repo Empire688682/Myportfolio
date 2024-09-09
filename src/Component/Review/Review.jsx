b1b4f7a1414dd3c9f5715e155ddd988c449a67f1
Empire688682 <asehindej@gmail.com>
2024-09-09 01:40:49 +0100

    swiper

--- src/Component/Review/Review.jsx
+++ src/Component/Review/Review.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import styles from './Review.module.css'
-// import Swiper core and required modules
+import styles from './Review.module.css';
 import { Navigation, Pagination, Autoplay, A11y } from 'swiper/modules';
-
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -15,37 +11,37 @@ import Image from 'next/image';
 
 const Review = () => {
     return (
-       <div className={styles.review}>
-         <Swiper
-        // install Swiper modules
-        modules={[Navigation, Pagination, A11y, Autoplay]}
-        spaceBetween={50}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: false }}
-        loop={true}
-        autoplay={{ 
-            delay: 3000, // Time between transitions (3000ms = 3 seconds)
-            disableOnInteraction: false, // Autoplay will not stop after user interactions
-          }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
-      >
-          <div className={styles.review_cart}>
-          {
-                AllReview.map((review) =>{
-                    return <SwiperSlide key={review.id} className={styles.review_slider}>
-                        <div className={styles.img_Con}><Image src={review.imageSrc} alt='' fill/></div>
-                        <h3>{review.name}</h3>
-                        <p>{review.body}</p>
-                    </SwiperSlide>
-                })
-            }
-          </div>
-      </Swiper>
-       </div>
-    )
-}
+        <div className={styles.review}>
+            <h1>Customer Reviews</h1>
+            <div className={styles.review_cart}>
+                <Swiper
+                    modules={[Navigation, Pagination, A11y, Autoplay]}
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    navigation
+                    pagination={{ clickable: true }}
+                    scrollbar={{ draggable: false }}
+                    loop={true}
+                    autoplay={{
+                        delay: 4000,
+                        disableOnInteraction: false,
+                    }}
+                >
+                    {AllReview.map((review) => (
+                        <SwiperSlide key={review.id} className={styles.review_slider}>
+                            <div className={styles.img_Con_Con}>
+                                <div className={styles.img_Con}>
+                                <Image src={review.imageSrc} alt='' fill />
+                                </div>
+                            </div>
+                            <h3>{review.name}</h3>
+                            <p>{review.body}</p>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+        </div>
+    );
+};
 
-export default Review
+export default Review;
