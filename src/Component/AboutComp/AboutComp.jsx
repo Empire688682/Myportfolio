35aa1d61b668ab7e52b721c74b837dcb94dd8644
Empire688682 <asehindej@gmail.com>
2024-09-20 17:48:50 +0100

    service

--- src/Component/AboutComp/AboutComp.jsx
+++ src/Component/AboutComp/AboutComp.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
-import styles from './AboutCom.module.css'
+import styles from './AboutCom.module.css';
+import Link from 'next/link';
 
 const AboutComp = () => {
   return (
@@ -16,7 +17,7 @@ const AboutComp = () => {
         <h3>ABOUT ME</h3>
         <h1>PERSONAL DETAILS</h1>
         <p>I'm a skilled Fullstack Web Developer and Software Developer with experience in JavaScript, PHP, Node.js, and database management, with expertise in frameworks like React, Next.js, Three.js, and more. I'm advancing my career step by step, always eager to learn and grow. As a fast learner, I collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life.</p>
-        <button>VIEW FULL DETAILS</button>
+        <Link href='/service' className={styles.linkBtn}>VIEW FULL DETAILS</Link>
         </div>
    </div>
   )
--- src/Component/ServiceComp/ServiceComp.jsx
+++ src/Component/ServiceComp/ServiceComp.jsx
@@ -6,43 +6,52 @@ const ServiceComp = () => {
     <div className={styles.container}>
       <div className={styles.conHead}>
         <h1>My Offered Services</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo error provident sunt quaerat temporibus cum cupiditate asperiores. Fugiat facilis dolorem, architecto repellendus corrupti nisi facere quia ipsa sunt neque explicabo?</p>
+        <p>At Jay-Empire, I’m dedicated to providing a comprehensive suite of services designed to elevate your online presence and drive your business forward
+
+</p>
       </div>
       <div className={styles.serviceBox}>
           <div className={styles.servicesCon}>
             <h2 className={styles.H2AsHead}> 
               <FaLaptopCode className={styles.icons} /> 
-              Web Dvelopment
+              Custom Web Development
               </h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita placeat voluptatum facilis dicta quod doloribus dolorum, sapiente obcaecati quaerat, atque dignissimos sed labore, asperiores velit in veniam ipsa similique nam.</p>
+            <p>I craft tailored solutions that meet your unique needs. Every website I build is a reflection of your brand, ensuring it stands out in the digital landscape.</p>
           </div>
           <div className={styles.servicesCon}>
             <h2 className={styles.H2AsHead}> 
               <FaLaptopCode className={styles.icons} /> 
-              Web Dvelopment
+              Responsive Design
               </h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita placeat voluptatum facilis dicta quod doloribus dolorum, sapiente obcaecati quaerat, atque dignissimos sed labore, asperiores velit in veniam ipsa similique nam.</p>
+            <p>Engaging and user-friendly designs that adapt seamlessly across all devices, enhancing user experience.</p>
           </div>
           <div className={styles.servicesCon}>
             <h2 className={styles.H2AsHead}> 
               <FaLaptopCode className={styles.icons} /> 
-              Web Dvelopment
+              E-commerce Solutions
               </h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita placeat voluptatum facilis dicta quod doloribus dolorum, sapiente obcaecati quaerat, atque dignissimos sed labore, asperiores velit in veniam ipsa similique nam.</p>
+            <p>Robust platforms that empower you to sell online effortlessly, complete with secure payment integrations.</p>
           </div>
           <div className={styles.servicesCon}>
             <h2 className={styles.H2AsHead}> 
               <FaLaptopCode className={styles.icons} /> 
-              Web Dvelopment
+              Maintenance & Support
               </h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita placeat voluptatum facilis dicta quod doloribus dolorum, sapiente obcaecati quaerat, atque dignissimos sed labore, asperiores velit in veniam ipsa similique nam.</p>
+            <p>Ongoing support to keep your website running smoothly, allowing you to focus on what you do best.</p>
           </div>
           <div className={styles.servicesCon}>
             <h2 className={styles.H2AsHead}> 
               <FaLaptopCode className={styles.icons} /> 
-              Web Dvelopment
+              Consultation Services
               </h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita placeat voluptatum facilis dicta quod doloribus dolorum, sapiente obcaecati quaerat, atque dignissimos sed labore, asperiores velit in veniam ipsa similique nam.</p>
+            <p>Unsure of where to start? I offer expert consultations to help you identify your needs and develop a strategic plan that aligns with your goals.</p>
+          </div>
+          <div className={styles.servicesCon}>
+            <h2 className={styles.H2AsHead}> 
+              <FaLaptopCode className={styles.icons} /> 
+              SEO Optimization
+              </h2>
+            <p>I implement best practices to enhance your website's visibility in search engines, driving organic traffic and helping your business reach a wider audience.</p>
           </div>
           <div className={styles.servicesCon}>
             <h2 className={styles.H2AsHead}> 
