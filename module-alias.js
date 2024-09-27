5c1df8a5f11c35d088718ab8224ecf9a41e9d383
Empire688682 <asehindej@gmail.com>
2024-09-27 13:34:56 +0100

    contact

--- module-alias.js
+++
@@ -1,4 +0,0 @@
-const moduleAlias = require('module-alias');
-const path = require('path');
-
-moduleAlias.addAlias('@', path.resolve(__dirname, 'src'));
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -5,76 +5,108 @@ import Link from 'next/link'
 import { GoHome } from "react-icons/go";
 import { BsTelephone } from "react-icons/bs";
 import { CiMail } from "react-icons/ci";
+import { useState } from 'react';
+import axios from 'axios';
 
 const Contact = () => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
+    subject: '',
     message: '',
   });
+  const [message, setMessage]= useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
   };
 
+  const sendEmail = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("api/emailSender", formData);
+      if (response.data.success) {
+        setFormData({
+          name: '',
+          email: '',
+          subject: '',
+          message: '',
+        });
+        setMessage(response.data.message)
+        setInterval(()=>{
+          setMessage('')
+        },3000);
+      }
+    } catch (error) {
+      console.log("ERROR:", error);
+      window.alert("An error occured")
+    }
+    finally{
+      setLoading(false);
+    }
+  }
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(formData);
+    sendEmail();
   };
 
   return (
     <div className={styles.container}>
-    <div className={styles.contactHead}>
-      <h1>Contact</h1>
-      <div className={styles.headLinks}>
-        <Link id='Link' href='/' style={{color:"white", textDecoration:"none"}}>Home</Link>
-        <><FaArrowRightLong  style={{width:"50px", color:"white"}} /></>
-        <Link id='Link' href='/contact' style={{color:"white", textDecoration:"none"}}>Contact</Link>
+      <div className={styles.contactHead}>
+        <h1>Contact</h1>
+        <div className={styles.headLinks}>
+          <Link id='Link' href='/' style={{ color: "white", textDecoration: "none" }}>Home</Link>
+          <><FaArrowRightLong style={{ width: "50px", color: "white" }} /></>
+          <Link id='Link' href='/contact' style={{ color: "white", textDecoration: "none" }}>Contact</Link>
+        </div>
       </div>
-    </div>
-    <div className={styles.map}>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.057579481107!2d3.365421073345873!3d6.639772193354738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b93906824f703%3A0x8cba4fcd8c840cd!2sOjodu%20Berger!5e0!3m2!1sen!2sng!4v1712595163454!5m2!1sen!2sng" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-    </div>
-    <div className={styles.contact}>
-          <div className={styles.contactLeft}>
-            <div className={styles.contactLeftCon}>
-            <GoHome  className={styles.icons} />
+      <div className={styles.map}>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.6980570086575!2d3.4764790736500792!3d6.559746622743857!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103bf27318f770c9%3A0x57a197a85265e736!2s13%20Kosebinu%20St%2C%20Ikorodu%2C%20Ibeshe%20104102%2C%20Lagos!5e0!3m2!1sen!2sng!4v1727440164951!5m2!1sen!2sng" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+      <div className={styles.contact}>
+        <div className={styles.contactLeft}>
+          <div className={styles.contactLeftCon}>
+            <GoHome className={styles.icons} />
             <div>
               <h2>Ikorodu Lagos, Nigeria</h2>
               <p>13 Koshebinu Street IbsheTitun</p>
             </div>
-            </div>
-            <div className={styles.contactLeftCon}>
+          </div>
+          <div className={styles.contactLeftCon}>
             <BsTelephone className={styles.icons} />
             <div>
               <h2><a href="tel:+234 9154 3581 35">+234 9154 3581 39</a></h2>
               <p>Mon to Sat 8am to 10pm</p>
             </div>
-            </div>
-            <div className={styles.contactLeftCon}>
+          </div>
+          <div className={styles.contactLeftCon}>
             <CiMail className={styles.icons} />
             <div>
               <h2><a href="mailto:asehindej@gmail.com">asehindej@gmail.com</a></h2>
               <p>Send us your query anytime!</p>
             </div>
+          </div>
+        </div>
+        <div className={styles.contactRight}>
+          <form onSubmit={handleSubmit}>
+            <div className={styles.inputs}>
+              <input type="text" name="name" value={formData.name} onChange={handleChange} id="name" placeholder='Enter your name' required />
+              <input type="mail" name="email" value={formData.email} onChange={handleChange} id="email" placeholder='Enter email address' required />
+              <input type="text" name="subject" value={formData.subject} onChange={handleChange} id="message" placeholder='Enter subject' required />
             </div>
-          </div>
-          <div className={styles.contactRight}>
-            <form onSubmit={handleSubmit}>
-             <div className={styles.inputs}>
-                <input type="text" name="name" onChange={handleChange} id="name" placeholder='Enter your name' required/>
-                <input type="mail" name="email" onChange={handleChange}  id="email" placeholder='Enter email address' required/>
-                <input type="text" name="message" onChange={handleChange}  id="message" placeholder='Enter subject' required/>
-             </div>
-              <div className={styles.textera}>
-              <textarea name="text" id="text" cols="30" rows="7" placeholder='Enter message' required></textarea>
-              <button type="submit">Send</button>
-              </div>
-            </form>
-          </div>
+            <div className={styles.textera}>
+              <textarea name="message" id="message" value={formData.message} onChange={handleChange} cols="30" rows="7" placeholder='Enter message' required></textarea>
+              {
+                message? <p>{message}</p>:null
+              }
+              <button type="submit">{loading ? "Sending..." : "Send" }</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-   </div>
   )
 }
 
