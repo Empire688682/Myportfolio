cceb3ec0511fce114711a3f00c5f7e0625f17f5a
Empire688682 <asehindej@gmail.com>
2024-10-02 22:28:48 +0100

    hero

--- public/reviewData.js
+++ public/reviewData.js
@@ -2,32 +2,27 @@
 export const AllReview = [
     {
         id: 1,
-        name: "Gloria Sam",
-        body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut aperiam ipsum odio ad et veritatis necessitatibus ratione, sint quis quae, eum aliquam provident illo possimus autem? Ducimus eaque molestias quaerat?",
-        imageSrc: "/review1.jpg",
+        name: "Sara Lee",
+        body: "I was skeptical about creating a website that could truly reflect the elegance of our product, but Juwon not only exceeded my expectations but also shifted my belief from relying on WhatsApp sellers to owning a stunning e-commerce site.",
+        imageSrc: "https://randomuser.me/api/portraits/women/9.jpg",
     },
     {
         id: 2,
-        name: "Samson Sam",
-        body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut aperiam ipsum odio ad et veritatis necessitatibus ratione, sint quis quae, eum aliquam provident illo possimus autem? Ducimus eaque molestias quaerat?",
-        imageSrc: "/review2.jpg",
+        name: "Mr Samson",
+        body: "I've never encountered a web developer as dedicated to their clients' success as Juwon.",
+        imageSrc: "https://randomuser.me/api/portraits/men/10.jpg",
     },
     {
         id: 3,
-        name: "Anabel Sam",
-        body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut aperiam ipsum odio ad et veritatis necessitatibus ratione, sint quis quae, eum aliquam provident illo possimus autem? Ducimus eaque molestias quaerat?",
+        name: "Emma Johnson",
+        body: "We never imagined our website could attract so much traffic until Juwon worked his magic. Highly recommend!",
         imageSrc: "/review3.jpg",
     },
     {
         id: 4,
         name: "Pascal Sam",
-        body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut aperiam ipsum odio ad et veritatis necessitatibus ratione, sint quis quae, eum aliquam provident illo possimus autem? Ducimus eaque molestias quaerat?",
+        body: "Juwon's attention to detail and commitment to our goals helped elevate our website beyond our expectations!",
         imageSrc: "/review4.jpg",
     },
-    {
-        id: 5,
-        name: "Jude Sam",
-        body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut aperiam ipsum odio ad et veritatis necessitatibus ratione, sint quis quae, eum aliquam provident illo possimus autem? Ducimus eaque molestias quaerat?",
-        imageSrc: "/review5.jpg",
-    }
+ 
 ];
