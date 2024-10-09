0915cea5f24f00fea4aedad2772e40cafb45e95b
Empire688682 <asehindej@gmail.com>
2024-10-09 21:57:02 +0100

    style

--- src/Component/AboutComp/AboutComp.jsx
+++ src/Component/AboutComp/AboutComp.jsx
@@ -22,8 +22,28 @@ const AboutComp = () => {
         <div className={styles.textCon}>
         <h3>ABOUT ME</h3>
         <h1>PERSONAL DETAILS</h1>
-        <p>I&#39;m a skilled Fullstack Web Developer and Software Developer with experience in JavaScript, PHP, Node.js, and database management, with expertise in frameworks like React, Next.js, Three.js, and more. I&#39;m advancing my career step by step, always eager to learn and grow. As a fast learner, I collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let&#39;s work together to bring your ideas to life.</p>
-        <Link href='/service' className={styles.linkBtn}>VIEW FULL DETAILS</Link>
+        <p>I&#39;m a Fullstack Web and Software Developer skilled in JavaScript, PHP, Node.js, and database management, with expertise in React, Next.js, and Three.js. I continuously advance my career by learning and growing. I work closely with clients to deliver efficient, scalable solutions that solve real-world problems. Let&#39;s bring your ideas to life.</p>
+        {
+          //<Link href='/service' className={styles.linkBtn}>VIEW FULL DETAILS</Link>
+        }
+        <div className={styles.more_details}>
+          <div>
+            <h2>PRESENT</h2>
+            <h4>Freelance Web Developer</h4>
+            <p>Working happily on my own projects</p>
+            <p>2023 - Present</p>
+          </div>
+          <div>
+            <h2>2023</h2>
+            <h4>Web development program</h4>
+            <p>Completed a web development program at DISCOVA</p>
+          </div>
+          <div>
+            <h2>2023-2024</h2>
+            <h4>Web development program at KNOWITTO</h4>
+            <p>Advance my web development program at KNOWITTO</p>
+          </div>
+        </div>
         </div>
    </div>
   )
