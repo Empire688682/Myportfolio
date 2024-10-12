6a85645afc66b54cb6ae9bc437941bde662c0a39
Empire688682 <asehindej@gmail.com>
2024-10-12 01:58:03 +0100

    about

--- src/Component/AboutComp/AboutComp.jsx
+++ src/Component/AboutComp/AboutComp.jsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import React from 'react'
+
+import React, { useState } from 'react'
 import styles from './AboutCom.module.css';
 import Link from 'next/link';
 import { SiReactos } from "react-icons/si";
@@ -8,58 +8,48 @@ import { FaFacebookF } from "react-icons/fa6";
 import { BsGithub, BsInstagram } from "react-icons/bs";
 import { SlSocialTwitter } from "react-icons/sl";
 import { CiLinkedin } from "react-icons/ci";
+import { CiCirclePlus, CiCircleMinus } from "react-icons/ci";
+import { AboutData } from '../AboutData/AboutData';
 
 const AboutComp = () => {
+  const [moreDetails, setMoreDetails] = useState("PRESENT");
   return (
-   <div className={styles.container}>
-        <div className={styles.imageB4}>
+    <div className={styles.container}>
+      <div className={styles.imageB4}>
         <div className={styles.animation}>
-          <SiReactos className={styles.icon}/>
-          <FaProjectDiagram className={styles.icon}/>
+          <Link href='www.linkedin.com/in/asehinde-juwon-73b04b268' className={styles.iconLink}><CiLinkedin style={{ width: "25px", height: "25px" }} /></Link>
+          <Link href='https://github.com/Empire688682' className={styles.iconLink}><BsGithub style={{ width: "25px", height: "25px" }} /></Link>
         </div>
-        <div className={styles.socialMedia}>
-        <h4>Let us be social</h4>
-        <div className={styles.links}>
-            <Link href='www.linkedin.com/in/asehinde-juwon-73b04b268' className={styles.iconLink}><CiLinkedin style={{width:"25px" ,height:"25px"}} /></Link>
-            <Link href='https://github.com/Empire688682' className={styles.iconLink}><BsGithub style={{width:"25px" ,height:"25px"}} /></Link>
-            <Link href='https://www.facebook.com/juwon.asehinde.7' className={styles.iconLink}><FaFacebookF style={{width:"25px" ,height:"25px"}} /></Link>
-            <Link href='https://www.instagram.com/jahwonempire/' className={styles.iconLink}><BsInstagram style={{width:"25px" ,height:"25px"}} /></Link>
-            <Link href='https://x.com/AsehindeJwon' className={styles.iconLink}><SlSocialTwitter style={{width:"25px" ,height:"25px"}} /></Link>
+        <div className={styles.hireMe_Con}>
+          <h4>Let make it possible</h4>
+          <div className={styles.links}>
+           <a className={styles.btn} href="mailto:asehindej@gmail.com">HIRE ME</a>
+          </div>
         </div>
       </div>
-        </div>
-        <div className={styles.textCon}>
+      <div className={styles.textCon}>
         <h3>ABOUT ME</h3>
         <h1>PERSONAL DETAILS</h1>
         <p>I&#39;m a Fullstack Web and Software Developer skilled in JavaScript, PHP, Node.js, and database management, with expertise in React, Next.js, and Three.js. I continuously advance my career by learning and growing. I work closely with clients to deliver efficient, scalable solutions that solve real-world problems. Let&#39;s bring your ideas to life.</p>
-        {
-          //<Link href='/service' className={styles.linkBtn}>VIEW FULL DETAILS</Link>
-        }
         <div className={styles.more_details}>
-          <div>
-            <h2>PRESENT</h2>
-            <h4>Freelance Web Developer</h4>
-            <p>Working happily on my own projects</p>
-            <p>2023 - Present</p>
-          </div>
-          <div>
-            <h2>2023</h2>
-            <h4>Web development program</h4>
-            <p>Completed a web development program at DISCOVA</p>
-          </div>
-          <div>
-            <h2>2023-2024</h2>
-            <h4>Web development program at KNOWITTO</h4>
-            <p>Advance my web development program at KNOWITTO</p>
-          </div>
-          <div>
-            <h2>Future plans</h2>
-            <h4>Senior Web Developer</h4>
-            <p>Plans of becoming a senior web developer working for both organisations and individuals</p>
-          </div>
+          <h2>More Details</h2>
+          {
+            AboutData.map((data) => (
+              <div key={data.title} className={styles.more_details_container}>
+                <div className={styles.more_details_header}> <h2>{data.title}</h2>{data.title === moreDetails? <CiCircleMinus className={styles.more_icon} onClick={()=> setMoreDetails("")} /> : <CiCirclePlus className={styles.more_icon} onClick={()=> setMoreDetails(data.title)} />}</div>
+                {
+                  moreDetails === data.title ? 
+                    <>
+                    <p>{data.text}</p>
+                    <p>{data.date}</p>
+                    </> : null
+                }
+              </div>
+            ))
+          }
         </div>
-        </div>
-   </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/Component/AboutData/AboutData.js
@@ -0,0 +1,27 @@
+export const AboutData = [
+    {
+        title: "PRESENT",
+        text: "Fullstack Freelance Web Developer",
+        date: "2024 - Present",
+    },
+    {
+        title: "PREVIOUS EXPERIENCE",
+        text: "Frontend Developer",
+        date: "2023 - 2024",
+    },
+    {
+        title: "EDUCATION",
+        text: "Online Courses - KNOWITTO, DISCOVA",
+        date: "2023",
+    },
+    {
+        title: "EXPERIENCE",
+        text: "Web Development Projects (Freelance)",
+        date: "2023 - Present",
+    },
+    {
+        title: "FUTURE GOALS",
+        text: "Aiming to work as a Full-time Developer in a Leading Organization",
+        date: "Future",
+    },
+];
