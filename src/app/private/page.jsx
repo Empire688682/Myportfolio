47f06d58a279e0c1bb9123442c301532a6cfb2b7
Empire688682 <asehindej@gmail.com>
2025-01-27 00:51:47 +0100

    back

--- src/app/private/page.jsx
+++ src/app/private/page.jsx
@@ -3,21 +3,57 @@ import React, { useState } from 'react';
 import style from "./Private.module.css";
 import { BiSolidHide } from "react-icons/bi";
 import { BiShow } from "react-icons/bi";
+import axios from 'axios';
 
 const Page = () => {
     const [showPassword, setShowPasswor] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const [data, setData] = useState({
+        email: "",
+        password: ""
+    });
+
+    const handleOnchange = (e) => {
+        const { name, value } = e.target;
+        setData((prev) => ({ ...prev, [name]: value }));
+    }
+
+    const registerUser = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.post("api/auth/signup", data);
+            if (response.success) {
+                setData({
+                    email: "",
+                    password: ""
+                })
+            }
+        } catch (error) {
+            console.log("Error:", error);
+        }
+        finally {
+            setLoading(false);
+        }
+    };
+
+    const handleSubmission = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <div className={style.private}>
             <h1>Welcome to your private room</h1>
             <form>
-                <input className={style.emailInput} type="email" name="email" placeholder='Email' />
+                <input className={style.emailInput} onChange={handleOnchange} type="email" value={data.email} name="email" placeholder='Email' required />
                 <div className={style.pwdCon}>
-                    <input className={style.pwdConInput} type={`${showPassword? "text":"password"}`} name="password" placeholder='Password' />
-                    <p onClick={()=>setShowPasswor(!showPassword)}>{
+                    <input className={style.pwdConInput} onChange={handleOnchange} value={data.password} type={`${showPassword ? "text" : "password"}`} name="password" placeholder='Password' required />
+                    <p onClick={() => setShowPasswor(!showPassword)}>{
                         showPassword ? <BiSolidHide /> : <BiShow />
                     }
                     </p>
                 </div>
+                <input className={style.submitInput} type="submit" value="FIRE" />
             </form>
         </div>
     )
