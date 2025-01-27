08bf39ac478dc7ba21858b11deeb2f17b81901ac
Empire688682 <asehindej@gmail.com>
2025-01-27 10:48:02 +0100

    admin

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -5,18 +5,30 @@ import Image from "next/image";
 
 const Page = () => {
   const [image, setImage] = useState(null);
+  const [data, setData] = useState({
+    image:"",
+    title:"",
+    category:"",
+    link:""
+  });
+
+  const handleOnchange = (e) =>{
+    const {name, value} = e.target;
+    setData((prev)=>({...prev, [name]:value}));
+  }
+
   return (
     <div className={style.container}>
-      <h1>Amin Panel</h1>
+      <h1>Admin Panel</h1>
       <div className={style.addDataContainer}>
         <form>
           <label htmlFor="image">
-            <Image src="/image_placeholder.webp" alt="" width={100} height={100}  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"/>
+            <Image src="/image_placeholder.webp" alt="" width={300} height={200}  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"/>
           </label>
-          <input type="text" name="title" placeholder="Title" required />
-          <input type="text" name="category" placeholder="Category" required/>
+          <input type="text" name="title" value={data.title} onChange={handleOnchange} placeholder="Title" required />
+          <input type="text" name="category" value={data.category} onChange={handleOnchange} placeholder="Category" required/>
           <input type="file" src="" alt="" id="image"  placeholder="Image" hidden/>
-          <input type="text" name="link" placeholder="link" required/>
+          <input type="text" name="link" value={data.link} onChange={handleOnchange} placeholder="link" required/>
           <button type="submit">Add Data</button>
         </form>
       </div>
