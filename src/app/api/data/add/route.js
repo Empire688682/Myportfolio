645fdf63d77867ccac33cf758f1b0df20cad6671
Empire688682 <asehindej@gmail.com>
2025-01-28 12:57:19 +0100

    buffer

--- src/app/api/data/add/route.js
+++ src/app/api/data/add/route.js
@@ -2,17 +2,22 @@ import { dataModel } from "../../models/dataModel";
 import { connectDb } from "../../utils/dbConnection";
 import { NextResponse } from "next/server";
 import path from "path";
-import fs from "fs";
+import fs from "fs/promises";
+import { cwd } from "process";
+import { arrayBuffer } from "stream/consumers";
 
 const addData = async (req) => {
     if (req.method === "POST") {
         try {
-            connectDb();
+            await connectDb();
             const formData = await req.formData();
             const image= formData.get("image");
             const title= formData.get("title");
             const category= formData.get("category");
             const link= formData.get("link");
+
+
+
         } catch (error) {
             console.log("Error:", error);
             return NextResponse.json({ success: false, message: "Unable to add data" }, { status: 500 })
