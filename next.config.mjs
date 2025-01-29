b50a792159541c4cc1f864b78d427df90e087405
Empire688682 <asehindej@gmail.com>
2025-01-29 17:51:28 +0100

    middleware

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // If you're exporting a static site
   images: {
     unoptimized: true, // Disable Next.js's image optimization
   },
