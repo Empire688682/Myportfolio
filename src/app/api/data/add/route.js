773e78b3f2c9e228dab82c54ebeea663f9c4c0ed
Empire688682 <asehindej@gmail.com>
2025-02-18 16:26:29 +0100

    resolved

--- src/app/api/data/add/route.js
+++ src/app/api/data/add/route.js
@@ -1,71 +1,68 @@
 import { dataModel } from "../../models/dataModel";
 import { connectDb } from "../../utils/dbConnection";
 import { NextResponse } from "next/server";
-import path from "path";
-import fs from "fs/promises";
+import IncomingForm from "formidable/Formidable";
 
 const addData = async (req) => {
   if (req.method === "POST") {
-    try {
-      await connectDb();
-      const formData = await req.formData();
-      const image = formData.get("image");
-      const title = formData.get("title");
-      const category = formData.get("category");
-      const link = formData.get("link");
+    const form = new IncomingForm();
+    return new Promise((resolve, reject) => {
+      form.parse(req, async (err, fields, files) => {
+        if (err) {
+          reject(NextResponse.json({ success: false, message: "Form parsing error" }, { status: 500 }))
+        }
+        try {
+          await connectDb();
+          const {title, category, link} = fields;
+          const image = files.image[0];
 
-      if (!image || !title || !category || !link) {
-        return NextResponse.json(
-          { success: false, message: "All field required" },
-          { status: 400 },
-        );
-      }
+          if (!image || !title || !category || !link) {
+            resolve(NextResponse.json(
+              { success: false, message: "All field required" },
+              { status: 400 },
+            ));
+          }
 
-      const validImageTypes = ["image/jpeg", "image/png", "image/webp"];
-      if (!validImageTypes.includes(image.type)) {
-        return NextResponse.json(
-          { success: false, message: "Invalid image type" },
-          { status: 400 },
-        );
-      }
+          const validImageTypes = ["image/jpeg", "image/png", "image/webp"];
+          if (!validImageTypes.includes(image.type)) {
+            resolve(NextResponse.json(
+              { success: false, message: "Invalid image type" },
+              { status: 400 },
+            ));
+          };
 
-      if (image.size > 1024 * 1024 * 5) {
-        return NextResponse.json(
-          { success: false, message: "Image size too large" },
-          { status: 400 },
-        );
-      }
+          if (image.size > 1024 * 1024 * 5) {
+            resolve(NextResponse.json(
+              { success: false, message: "Image size too large" },
+              { status: 400 },
+            ));
+          };
 
-      const safeImage = image.name.replace(/[^a-zA-Z0-9.-_]/g, "");
+          const safeImage = image.name.replace(/[^a-zA-Z0-9.-_]/g, "");
 
-      const imageName = `${Date.now()}_${safeImage}`;
+          const imageName = `${Date.now()}_${safeImage}`;
 
-      const imagePath = path.join(process.cwd(), "public", "images");
 
-      const imageDir = path.join(imagePath, imageName);
-      await fs.mkdir(imagePath, { recursive: true });
-
-      const buffer = Buffer.from(await image.arrayBuffer());
-      await fs.writeFile(imageDir, buffer);
-
-      const newData = new dataModel({
-        image: `/images/${imageName}`,
-        title,
-        category,
-        link,
-      });
-      await newData.save();
-      return NextResponse.json(
-        { success: true, data: newData, message: "Data added successfully" },
-        { status: 200 },
-      );
-    } catch (error) {
-      console.log("Error:", error);
-      return NextResponse.json(
-        { success: false, message: "Unable to add data" },
-        { status: 500 },
-      );
-    }
+          const newData = new dataModel({
+            image: `${imageName}`,
+            title,
+            category,
+            link,
+          });
+          await newData.save();
+          resolve(NextResponse.json(
+            { success: true, data: newData, message: "Data added successfully" },
+            { status: 200 },
+          ));
+        } catch (error) {
+          console.log("Error:", error);
+          resolve(NextResponse.json(
+            { success: false, message: "Unable to add data" },
+            { status: 500 },
+          ));
+        }
+      })
+    })
   }
 };
 
